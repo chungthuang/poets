e3a6908c511ff81941716f2d3a77139b2248f312
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-03-04 20:21:18 +0000

    cthuang: Create docker-compose stack to run validators

---
+++ Tiltfile
@@ -0,0 +1,2 @@
+local_resource(name="parachain-collator", serve_cmd="./scripts/run-collator.sh")
+docker_compose("./docker-compose.yml")
--- scripts/run-collator.sh
+++ scripts/run-collator.sh
@@ -3,12 +3,12 @@
     --alice \
     --collator \
     --force-authoring \
-    --chain raw-parachain-chainspec.json \
+    --chain ./specs/raw-parachain-chainspec.json \
     --base-path /tmp/parachain/alice \
     --port 40333 \
     --ws-port 8844 \
     -- \
     --execution wasm \
-    --chain raw-local-chainspec.json \
+    --chain ./specs/raw-local-chainspec.json \
     --port 30343 \
     --ws-port 9977
